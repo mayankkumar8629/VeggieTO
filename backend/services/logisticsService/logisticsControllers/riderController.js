5c4f4ecd06b358e5880749418566573f31a495bc
Mayank Kumar <mayankkumarverma306@gmail.com>
2025-05-23 23:29:54 +0530

    creted all the api for the rider from assigning the delivery to order delivered

--- backend/services/logisticsService/logisticsControllers/riderController.js
+++ backend/services/logisticsService/logisticsControllers/riderController.js
@@ -3,6 +3,8 @@ import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 dotenv.config({path:"../../../.env"});
 import jwt from "jsonwebtoken";
+import { notifyCustomer } from "../config/websocket.js";
+import Delivery from "../../../UserModel/delivery.model.js";
 
 export const riderSignup = async(req,res)=>{
     try{
@@ -67,5 +69,114 @@ export const riderLogin = async(req,res)=>{
     }catch(error){
         console.error("Error in riderLogin:",error);
         return res.status(500).json({message:"Internal server error"});
+    }
+}
+export const riderDeliveryAccept = async(req,res)=>{
+    const {deliveryId}=req.body;
+    const riderId = req.user.riderId;
+    try{
+        if(!deliveryId){
+            return res.status(400).json({message:"Please provide deliveryId"});
+        }
+        const rider = await Rider.findById(riderId);
+        if(!rider){
+            return res.status(400).json({message:"Rider not found"});
+        }
+        const delivery = await Delivery.findOneAndUpdate(
+            {_id:deliveryId,
+                status:"pending"
+            },
+            {
+                status:"assigned",
+                rider:riderId
+            },
+            {new :true}
+        ).populate("user","name address contactNumber");
+        if(!delivery){
+            return res.status(400).json({message:"Delivery not found or already assigned"});
+        }
+        //notifying the customer through websockt
+        notifyCustomer(delivery.user._id,"delivery_assigned",{
+            deliveryId,
+            message:"Your delivery has been assigned to a rider",
+            riderName:rider.name,
+            status:delivery.status,
+            address:delivery.user.address
+        })
+        return res.status(200).json({
+            message:"Delivey assigned successfully",
+            delivery
+        })
+    }catch(error){
+        console.error("Error in riderDeliveryAccept:",error);
+        return res.status(500).json({message:"Internal server error"});
+    }
+}
+///when rider picks up the order 
+export const riderDeliveryPickup = async(req,res)=>{
+    const {deliveryId}=req.body;
+    const riderId = req.user.riderId;
+    try{
+        const delivery = await Delivery.findOneAndUpdate(
+            {
+                _id:deliveryId,
+                rider:riderId,
+                status:"assigned"
+            },
+            {
+                status:"picked-up"
+            },
+            {new:true}
+        ).populate("user","name address contactNumber");
+        if(!delivery){
+            return res.status(400).json({message:"Delivery not found or already picked up"});
+        }
+
+        //notifying the customer
+        notifyCustomer(delivery.user._id,"delivery_picked_up",{
+            deliveryId,
+            messsage:"Your delivery has been picked up by the rider",
+            riderName:delivery.rider.name
+        })
+        return res.status(200).json({
+            message:"Delivery picked up successfully",
+            delivery
+        });
+    }catch(error){
+        console.error("Error in riderDeliveryPickup:",error);
+        return res.status(500).json({message:"Internal server error"});
+    }
+}
+//delievery completed
+export const riderDeliveryComplete = async(req,res)=>{
+    const {deliveryId}=req.body;
+    const riderId = req.user.riderId;
+    try{
+        const delivery = await Delivery.findOneAndUpdate(
+            {
+                _id:deliveryId,
+                rider:riderId,
+                status:"picked-up"
+            },
+            {
+                status:"delivered"
+            },
+            {
+                new:true
+            }
+        ).populate("user","name address contactNumber");
+        if(!delivery){
+            return res.status(400).json({message:"Delivery not found or already delivered"});
+        }
+        //notifying the customer
+        notifyCustomer(delivery.user._id,"delivery_delivered",{
+            deliveryId,
+            message:"Your delivery has been delivered",
+            riderName:delivery.rider.name
+        })
+        return res.status(200).json({
+            message:"Delivery completed successfully",
+            delivery
+        });
     }
 }
