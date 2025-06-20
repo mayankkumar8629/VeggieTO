882840c8fa6bbf4c1c1a3ad1ec9c44212f3c7630
Mayank Kumar <mayankkumarverma306@gmail.com>
2025-06-20 20:32:37 +0530

    configured the delivry partner in the socket

--- backend/services/logisticsService/logisticsControllers/deliveryPartner.controller.js
+++ backend/services/logisticsService/logisticsControllers/deliveryPartner.controller.js
@@ -1,6 +1,9 @@
+import { JsonWebTokenError } from "jsonwebtoken";
 import DeliveryPartner from "../../../adminModel/deliveryPartner.model.js";
 import bcrypt from "bcrypt";
-
+import dotenv from "dotenv";
+dotenv.config({path:"../../../.env"});
+import jwt from "jsonwebtoken";
 
 //delivery partner signup
 export const  signup = async(req,res)=>{
@@ -37,6 +40,40 @@ export const  signup = async(req,res)=>{
         });
     }catch(error){
         console.error("Error in delivery partner signup:", error);
+        return res.status(500).json({message:"Internal server error"});
+    }
+}
+//delivery partner login
+export const login = async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({message:"Please fill all the fields"});
+        }
+        const user = await DeliveryPartner.findOne({emaail:email});
+        if(!user){
+            return res.status(400).json({message:"Delivery partner not found"});
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({message:"Password does not match"});
+        }
+        const token = jwt.sign(
+            {id:user._id,email:user.email,role:user.role},
+            process.env.JWT_SECRET,
+            {expiresIn:"1d"}
+        )
+        return res.status(200).json({
+            success:true,
+            message:"Logged in successfully",
+            token,
+            deliveryPartner:{
+                name:user.name,
+                email:user.email
+            }
+        });
+    }catch(error){
+        console.error("Error in login ",error);
         return res.status(500).json({message:"Internal server error"});
     }
 }
