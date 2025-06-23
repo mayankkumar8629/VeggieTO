590e82ec5160ffb4b389adcd9728b7f3eb393d57
Mayank Kumar <mayankkumarverma306@gmail.com>
2025-06-23 14:47:17 +0530

    merged the conflict

--- backend/services/logisticsService/logisticsRoutes/deliveryPartnerRoute.js
+++ backend/services/logisticsService/logisticsRoutes/deliveryPartnerRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import {signup,login,acceptShipment,shipmentOnTheWay,orderDelivered} from "../logisticsControllers/deliveryPartner.controller.js";
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 const router=express.Router();
 
 router.post("/signup",signup);
 router.post("/login",login);
-router.post("accept-shipment",acceptShipment);
-router.post("/shipment-on-the-way",shipmentOnTheWay);
-router.post("/order-delivered",orderDelivered);
+router.post("accept-shipment",authenticateToken,acceptShipment);
+router.post("/shipment-on-the-way",authenticateToken,shipmentOnTheWay);
+router.post("/order-delivered",authenticateToken,orderDelivered);
 
 export default router;
--- backend/services/logisticsService/logisticsRoutes/listingRoute.js
+++ backend/services/logisticsService/logisticsRoutes/listingRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { actionOnProductListing } from "../logisticsControllers/listingControllers";
+import { authenticateToken } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.post("/action",actionOnProductListing);
+router.post("/action",authenticateToken,actionOnProductListing);
 
 export default router;
