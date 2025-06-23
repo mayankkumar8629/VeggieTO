324128a5db6a45e11501a6732b3f52ad135b1284
Adit7643 <adityakumarjmp15@gmail.com>
2025-06-23 22:56:35 +0530

    added socket

--- backend/services/logisticsService/config/websocket.js
+++ backend/services/logisticsService/config/websocket.js
@@ -1,7 +1,7 @@
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-dotenv.config({path:"../../../.env"});
+dotenv.config({ path: "../../../.env" });
 
 export const activeConnections = {
     riders: new Map(),
@@ -10,10 +10,10 @@ export const activeConnections = {
     farmers: new Map()
 }
 
-export function initSocket(server){
-    const io = new Server(server,{
+export function initSocket(server) {
+    const io = new Server(server, {
         cors: {
-            origin: "*", // Allow all origins (for development)
+            origin: "http://localhost:5173", // Allow all origins (for development)
             methods: ["GET", "POST"],
             allowedHeaders: ["Authorization"],
             credentials: true
@@ -21,116 +21,116 @@ export function initSocket(server){
     });
 
     //authentication middleware
-    io.use((socket,next)=>{
+    io.use((socket, next) => {
         const token = socket.handshake.auth.token;
-        console.log("Token:",token);
-        if(!token){
+        console.log("Token:", token);
+        if (!token) {
             return next(new Error("Authentication error"))
         }
-        try{
-            const {id,role}= jwt.verify(token,process.env.JWT_SECRET);
-            socket.id=id;
-            socket.role=role;
-            console.log("User ID:",id);
-            console.log("Role:",role);
-            
+        try {
+            const { id, role } = jwt.verify(token, process.env.JWT_SECRET);
+            socket.id = id;
+            socket.role = role;
+            console.log("User ID:", id);
+            console.log("Role:", role);
+
             next();
-        }catch(error){
-            console.error("Authentication error:",error);
+        } catch (error) {
+            console.error("Authentication error:", error);
             next(new Error("Authentication error"));
         }
     })
     //connection handler
-    io.on('connection',(socket)=>{
-       
-        const {id,role}=socket;
+    io.on('connection', (socket) => {
+
+        const { id, role } = socket;
         //handling rider connection
-        if(role==="rider"){
-            activeConnections.riders.set(id,{
+        if (role === "rider") {
+            activeConnections.riders.set(id, {
                 socket,
-                isAvailable:true
+                isAvailable: true
             });
-            console.log('Rider connected:',id);
+            console.log('Rider connected:', id);
         }
         //handling customer connection
-        else if(role==="customer"){
-            activeConnections.customers.set(id,socket);
-            console.log('Customer connected:',id);
-        }else if(role ==="delivery-partner"){
-            activeConnections.deliveryPartners.set(id,{
+        else if (role === "customer") {
+            activeConnections.customers.set(id, socket);
+            console.log('Customer connected:', id);
+        } else if (role === "delivery-partner") {
+            activeConnections.deliveryPartners.set(id, {
                 socket,
-                isAvailable:true
+                isAvailable: true
             });
-            console.log("Delivery Partner connected:",id);
-        }else if(role==="farmer"){
-            activeConnections.farmers.set(id,socket);
-            console.log("Farmer connected:",id);
+            console.log("Delivery Partner connected:", id);
+        } else if (role === "farmer") {
+            activeConnections.farmers.set(id, socket);
+            console.log("Farmer connected:", id);
         }
 
         //handling avalablity of riders and delivery partners
-        if(role === "rider" || role === "delivery-partner"){
-            socket.on('set_availability',(available)=>{
-                
-                const connectionMap = role==='rider'
-                ?activeConnections.riders     
-                :activeConnections.deliveryPartners;
+        if (role === "rider" || role === "delivery-partner") {
+            socket.on('set_availability', (available) => {
 
-                const user= connectionMap.get(id);
-                if(user){
-                    user.isAvailable=available;
+                const connectionMap = role === 'rider'
+                    ? activeConnections.riders
+                    : activeConnections.deliveryPartners;
+
+                const user = connectionMap.get(id);
+                if (user) {
+                    user.isAvailable = available;
                 }
             });
         }
-       
+
         //handling disconnection
-        socket.on('disconnect',()=>{
-           if(role==="rider"){
+        socket.on('disconnect', () => {
+            if (role === "rider") {
                 activeConnections.riders.delete(id);
-                console.log("Rider disconnected:",id);
-           }else if(role ==="customer"){
+                console.log("Rider disconnected:", id);
+            } else if (role === "customer") {
                 activeConnections.customers.delete(id);
-                console.log("Customer disconnected:",id);
-           }else if(role === "delivery-partner"){
+                console.log("Customer disconnected:", id);
+            } else if (role === "delivery-partner") {
                 activeConnections.deliveryPartners.delete(id);
-                console.log("Delivery Partner :",id);
-           }
+                console.log("Delivery Partner :", id);
+            }
         });
 
     });
 }
 //function to notify the customer
-export function notifyCustomer (customerId,event,data){
+export function notifyCustomer(customerId, event, data) {
     const customerSocket = activeConnections.customers.get(customerId);
-    if(customerSocket){
-        try{
-            customerSocket.emit(event,data);
-        }catch(error){
-            console.error("Error notifying customer",error);
+    if (customerSocket) {
+        try {
+            customerSocket.emit(event, data);
+        } catch (error) {
+            console.error("Error notifying customer", error);
         }
     }
 }
 //function to notify all the farmers
-export function notifyFarmers(farmerId,event,data){
-    const farmerSocket=activeConnections.farmers.get(farmerId);
-    if(farmerSocket){
-        try{
-            farmerSocket.emit(event,data);
-        }catch(error){
-            console.error("Error notifying farmer",error);
+export function notifyFarmers(farmerId, event, data) {
+    const farmerSocket = activeConnections.farmers.get(farmerId);
+    if (farmerSocket) {
+        try {
+            farmerSocket.emit(event, data);
+        } catch (error) {
+            console.error("Error notifying farmer", error);
         }
     }
 }
 //function to notify all the ridrs
-export function notifyAvailableRiders(event,data){
-    let count=0;
+export function notifyAvailableRiders(event, data) {
+    let count = 0;
 
-    activeConnections.riders.forEach((rider)=>{
-        if(rider.isAvailable){
-            try{
-                rider.socket.emit(event,data)
+    activeConnections.riders.forEach((rider) => {
+        if (rider.isAvailable) {
+            try {
+                rider.socket.emit(event, data)
                 count++;
-            }catch(error){
-                console.error("Error notifying rider",error);
+            } catch (error) {
+                console.error("Error notifying rider", error);
             }
         }
     })
@@ -138,11 +138,11 @@ export function notifyAvailableRiders(event,data){
 }
 
 //functin to notify all the delivery partners
-export function notifyAvailableDeliveryPartners(event,data){
-    let notifiedCount=0;
-    activeConnections.deliveryPartners.forEach((partner)=>{
-        if(partner.isAvailable){
-            partner.socket.emit(event,data);
+export function notifyAvailableDeliveryPartners(event, data) {
+    let notifiedCount = 0;
+    activeConnections.deliveryPartners.forEach((partner) => {
+        if (partner.isAvailable) {
+            partner.socket.emit(event, data);
             notifiedCount++
         }
     });
